21ce0f291b78fb7b1c606c9642cf4f9d3625b734
PRAVEENKUMAR2000M git config --global user.PRAVEENKUMAR2000M git config --global user.name PRAVEENKUMAR2000M <praveenmech483@gmail.com>
2023-09-25 11:39:47 +0530

    first commit

--- day13/dom.js
+++ day13/dom.js
@@ -77,31 +77,74 @@
 
 
 
-// //04)exercise
-// let anchortag = document.createElement('a');
-// anchortag.href = './png-transparent-google-logo-google-search-google-play-google-text-logo-number.png'
-// paragraph.textContent = 'click here'
-// paragraph.target(_blank);
-
-// document.getElementById("paragrapg").appendChild(anchortag);
 
 
 
+// // Create a new anchor element
+// let anchor = document.createElement("a");
 
-// Create a new anchor element
-let anchor = document.createElement("a");
+// // Set its 'href' attribute to 'https://www.google.com'
+// anchor.href = "https://www.google.com/";
 
-// Set its 'href' attribute to 'https://www.google.com'
-anchor.href = "https://www.google.com/";
+// // Set its 'target' attribute to '_blank'
+// anchor.textContent = 'chick here'
 
-// Set its 'target' attribute to '_blank'
-anchor.target = "_blank";
+// anchor.target = "_blank";
 
-// Create a new paragraph element
-let paragraph = document.createElement("p");
+// // Create a new paragraph element
+// let paragraph = document.createElement("p");
 
-// Append the anchor element to the paragraph element
-paragraph.appendChild(anchor);
+// // Append the anchor element to the paragraph element
+// paragraph.appendChild(anchor);
 
-// Append the paragraph element to the document body or any other desired location
-document.body.appendChild(paragraph);
+// // Append the paragraph element to the document body or any other desired location
+// document.body.appendChild(paragraph);
+
+
+
+// let anchor = document.createElement("a");
+// paragraph.append(anchor);
+// document.body.appendChild(paragraph);anchor.href = "https://www.google.com/"
+// anchor.textContent = "click here";
+// anchor.target = "_blank";
+// let paragraph = document.createElement("p");
+
+
+// let list = document.createElement("ul");
+
+// let listItem1 = document.createElement("li");
+// listItem1.textContent = "item 1";
+// list.appendChild(listItem1);
+
+// let listItem2 = document.createElement("li");
+// listItem2.textContent = "item 2";
+// list.appendChild(listItem2);
+
+// document.body.append(list);
+
+
+
+
+
+let list = document.createElement("ul");
+let listItem1 = document.createElement("li");
+listItem1.textContent = "item 1";
+list.appendChild(listItem1);
+document.body.append(listItem1);
+
+let listItem2 = document.createElement("li");
+listItem2.textContent = "item 2";
+list.appendChild(listItem2);
+document.body.append(listItem2);
+
+
+let listItem3 = document.createElement("li")
+listItem3.textContent = "item3"
+list.appendChild(listItem3);
+document.body.append(list);
+
+
+
+
+
+
