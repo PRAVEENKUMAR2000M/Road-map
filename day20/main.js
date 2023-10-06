d2e8dc99b8d9d980facc07ed461b297bc3c39a61
PRAVEENKUMAR2000M git config --global user.PRAVEENKUMAR2000M git config --global user.name PRAVEENKUMAR2000M <praveenmech483@gmail.com>
2023-10-06 12:20:44 +0530

    confirm

---
+++ day20/main.js
@@ -0,0 +1,6 @@
+let {Student, mentor} = require("./react1.js");
+let Student101 = new Student()
+let mentor101 = new mentor();
+
+Student101.sayHello();
+mentor101.sayHello();
---
+++ day20/react1.js
@@ -0,0 +1,135 @@
+// let x = 10;
+
+// if (true) {
+//     x = 20;
+// }
+
+// console.log(x);
+
+
+// let x = 10;
+
+// function scope(){
+    
+//     x = 50;
+// }
+// scope();
+// console.log(x);
+
+
+
+// const arrays = [1, 2, 3, 4, 5];
+// arrays[2] = 12;
+// console.log(arrays);
+
+
+//  let func=()=> {
+//     console.log("hello");
+// }
+// func();
+
+
+// function func() {
+//     console.log("hello")
+// }
+// func();
+
+
+
+// non anonymous function
+// function sayHello() {
+//     console.log("hello world");
+// }
+// sayHello();
+
+//anonymous function
+
+// let sayHello = function () {
+//     console.log("hello world");
+// }
+// sayHello();
+
+
+
+//arrow funtion
+// let sayHello = () => {
+//     console.log("hello world");
+// }
+// sayHello();
+
+
+
+// IIFE: immediateley invoked function expression
+
+// (() => {
+//     console.log("hello world");
+// })();
+
+
+// classes
+// class Student{
+//     constructor(name = "guest", age) {
+//         this.name = name
+//         this.age = age;
+//      }
+// }
+ 
+// let Student101 = new Student("praveen", 26)
+// let Student102 = new Student("kumar", 25)
+// let Student103 = new Student()
+
+// console.log(Student101);
+// console.log(Student102);
+// console.log(Student103);
+
+
+
+// class Student{
+//     sayHello() {
+//         console.log("hello mentors");
+//     }
+// }
+
+// class mentor{
+//     sayHello() {
+//         console.log("hello students")
+//     }
+// }
+
+
+// module.exports = {
+//     Student, mentor
+// }
+
+
+//template literals
+
+let name = "praveen"
+let age = 24;
+let message = `hello my ${name} and iam ${age} old`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
