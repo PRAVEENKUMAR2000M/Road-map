d30c7a5a63239d2fcc1da9da77d17531e5796692
PRAVEENKUMAR2000M git config --global user.PRAVEENKUMAR2000M git config --global user.name PRAVEENKUMAR2000M <praveenmech483@gmail.com>
2023-10-16 13:14:34 +0530

    first commit

--- day20/react1.js
+++ day20/react1.js
@@ -104,12 +104,133 @@
 
 //template litera
 
-let name = "praveen"
-let age = 24;
-// let message = `hello my ${name} and iam ${age} old`;
+// let name = "praveen"
+// let age = 24;
+// let message = `hello iam ${name} and iam ${age} years old`
+// console.log(message);
 
-let message = "hello iam"  +  "24 years old"
-console.log(message);
+
+
+
+//spread operator
+
+// let numbers = [1, 2, 3, 4];
+// let moreNumbers = [...numbers, 5, 6, 7];
+// console.log(moreNumbers);
+
+
+// let array1 = [1, 2, 3];
+// let arrat2 = [4, 5, 6];
+// combainedArray = [...array1, ...arrat2];
+// console.log(combainedArray);
+
+
+
+
+// let object1 = {
+//     x: 10,
+//     y:20
+// }
+// // console.log({...object1})
+
+// let object2 = {
+//    z:30
+// };
+
+// console.log({ ...object1, ...object2 });
+
+
+
+
+// // rest operator
+// function display(a, b, ...rest) {
+//     console.log(a, b, rest);
+// }
+
+// display(5, 7, 8, 9, 10);
+
+
+
+
+
+
+//destructuring method
+
+// simple interest
+
+// let SI = [1000, 2, 5];
+// let [principleofamount, numbersofyears, rateofinterest] = SI
+// console.log(principleofamount, numbersofyears, rateofinterest);
+
+// console.log(formula);
+
+
+
+// let SI = [1000, 2, 5];
+// let principleAmount = SI[0];
+// let numbersOfyears = SI[1];
+// let rateOfinterest = SI[2];
+// console.log(principleAmount, numbersOfyears, rateOfinterest);
+
+
+
+
+// let numbers = [1, 2, 3, 4, 5];
+// // let [, , , c, d, e] = numbers;
+// let [d,e]=numbers.slice(3,)
+// console.log( d, e);
+
+
+
+// let a = 6;
+// let b = 5;
+// console.log(`before swaping b = ${ a }, a = ${ b }`);
+
+
+
+// var num = "12 13 14 15".split(" ").map(Number);
+// console.log(num);
+
+
+
+// let num = '1 2 3 4 5 6 7 8 9 ';
+// for (let i = 0; i <num.length; i++){
+//     if (num[i] % 2 != 0) {
+//         console.log(num[i]);
+//     }
+// }
+
+
+
+// let num = "1 2 3 4 5 6 7 8 9".split(" ");
+// for (let i = 0; i < num.length; i++){
+//     if (num[i]%2==0) {
+//         console.log(num[i]);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ practice/practice.js
@@ -0,0 +1,135 @@
+/*
+let num = "5 5"
+let num1 = parseInt(num[0]);
+let num2 = parseInt(num[1]);
+if (num1 % 2 == 0 && num2 % 2 == 0) {
+    console.log("even")
+} else {
+    console.log("odd");
+}
+*/
+
+
+
+
+// let num = "2";
+// let temp = [];
+// for (let i = 1; i <=num; i++){
+//     temp.push(i * 2);
+// }
+// console.log(temp.join(" "));
+
+
+
+// let num = 2;
+// let temp = [];
+
+// for (let i = 1; i <= 3; i++) {
+//     temp.push(i * 2);
+// }
+
+// console.log(temp.join(" "));
+
+
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i<num.length ; i++){
+//     if (num[i] % 2 != 0) {
+//         console.log(num[i])
+//     }
+// }
+
+// let num = ["1 2 3 4 5 "].toString(" ")
+// console.log(num);
+
+
+
+
+// let num = ["5 5"];
+// let num1 = parseInt(num[0]);
+// let num2 = parseInt(num[1]);
+// if (num1 % 2 == 0 && num2 % 2 == 0) {
+//     console.log("even");
+// } else {
+//     console.log("odd");
+// }
+
+
+
+// let radius = 2;
+// // let cir;
+// if (radius < 0) {
+//     console.log("error");
+// } else {
+//     let cir = 2 * 3.14 * radius
+//     console.log(cir.toFixed(2))
+// }
+
+
+
+// let num = 5;
+// if (num > 0) {
+//     console.log("positive")
+// } else {
+//     console.log("negative")
+// }
+
+
+
+// let numbers = [50, 60, 70];
+// let largest = numbers[0];
+// for (i = 0; i < numbers.length; i++){
+//     if (numbers[i] > largest) {
+//         largest = numbers[i];
+//     }
+// }
+// console.log(largest);
+
+
+
+
+
+// // Step 1: Get input from the user
+// var N = parseInt([3])
+
+// temp = [];
+
+// // Step 2: Check if N is 0
+// if (N === 0) {
+//     console.log("NULL");
+// } else if (N > 0) {
+//     // Step 3: Loop from 1 to N
+//     for (var i = 1; i <= N; i++) {
+//         temp.push(9 * i);
+//         console.log(temp.join(" "));
+//     }
+// }
+
+
+
+// Step 1: Get input from the user
+
+// var N = parseInt([3]);
+
+// Step 2: Check if N is 0
+// if (N === 0) {
+//     console.log("NULL");
+// } else if (N > 0) {
+//     // Initialize an empty string to hold the result
+//     var result = "";
+
+//     // Step 3: Loop from 1 to N
+//     for (var i = 1; i <= N; i++) {
+//         // Step 4: Calculate the multiple of 9
+//         var multipleOf9 = 9 * i;
+
+//         // Step 5: Append the multipleOf9 to the result with a space
+//         result = result+multipleOf9 + " ";
+//     }
+
+//     // Step 6: Print the result without an additional newline
+//     console.log(result);
+// }
+
+
+
