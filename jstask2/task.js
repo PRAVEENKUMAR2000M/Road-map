a71d91b0bd01d37784f21219ae80926bceccbdda
praveen <praveenmech483@gmail.com>
2023-10-30 08:16:34 +0530

    task completed

---
+++ jstask2/task.js
@@ -0,0 +1,62 @@
+//Scope and Purpose:
+
+//Document Object:
+
+     /*The Document object, often accessed through the document variable in JavaScript, represents the content and structure of a web page.It focuses on the HTML document and its elements, allowing you to interact with and manipulate the page's content.*/
+
+//Window Object:
+   
+    /*The Window object, usually accessed through the window variable in JavaScript, represents the browser window or tab.It encompasses various functionalities related to the browser itself, including navigation, global variables, and controlling the browser window.*/
+    
+//Hierarchy:
+
+    /*The Document object is a child of the Window object in the DOM hierarchy. This means that the Document object exists within the context of the browser window. The Window object is at the top of the JavaScript scope chain, and it can access the Document object and control it.*/
+
+
+//Content vs. Browser:
+
+//Document Object:
+
+    /*The Document object is primarily concerned with the content and structure of the web page.It provides methods and properties for accessing, modifying, and interacting with the HTML elements and content on the page.*/
+
+
+//Window Object:
+
+    /*The Window object deals with browser - level functionality, such as controlling navigation(opening, closing, and navigating browser tabs or windows), managing browser history, setting timers and intervals for executing JavaScript code, and handling global variables.*/
+
+
+//Examples of Use:
+
+    /*You use the Document object to access and manipulate elements on a web page, such as changing text content, adding or removing elements, and attaching event listeners to respond to user interactions.*/
+    
+    /*The Window object is used for tasks like opening new browser windows or tabs, redirecting to different URLs, managing the browser's history, setting timeouts and intervals for code execution, and accessing global variables and functions.*/
+
+
+//Document Object:
+ 
+     /*The Document object, often accessed through the document variable in JavaScript, represents the content and structure of a web page.It focuses on the HTML document and its elements, allowing you to interact with and manipulate the page's content.*/
+
+//Window Object:
+
+     /*The Window object, usually accessed through the window variable in JavaScript, represents the browser window or tab.It encompasses various functionalities related to the browser itself, including navigation, global variables, and controlling the browser window.*/
+
+
+//Hierarchy:
+
+     /*The Document object is a child of the Window object in the DOM hierarchy. This means that the Document object exists within the context of the browser window. The Window object is at the top of the JavaScript scope chain, and it can access the Document object and control it.
+Content vs. Browser:*/
+
+//Document Object:
+
+    /*The Document object is primarily concerned with the content and structure of the web page.It provides methods and properties for accessing, modifying, and interacting with the HTML elements and content on the page.*/
+
+//Window Object:
+
+    /*The Window object deals with browser - level functionality, such as controlling navigation(opening, closing, and navigating browser tabs or windows), managing browser history, setting timers and intervals for executing JavaScript code, and handling global variables.*/
+
+
+//Examples of Use:
+
+    /*You use the Document object to access and manipulate elements on a web page, such as changing text content, adding or removing elements, and attaching event listeners to respond to user interactions.*/
+
+    /*The Window object is used for tasks like opening new browser windows or tabs, redirecting to different URLs, managing the browser's history, setting timeouts and intervals for code execution, and accessing global variables and functions.*/
